d8a41926ade8fe1964fc13941c2171ea40027951
nesun3 <surneogi14@gmail.com>
2023-09-28 18:26:03 +0530

    second commit

--- src/Get file in Github/src/main/resources/script/script1.groovy
+++ src/Get file in Github/src/main/resources/script/script1.groovy
@@ -9,7 +9,7 @@ def Message processData(Message message) {
     def body = message.getBody(java.lang.String)
     def messageLog = messageLogFactory.getMessageLog(message)
     if (messageLog != null) {
-        messageLog.addAttachmentAsString('GITHUB RESPONSE', body, 'text/plain')
+        messageLog.addAttachmentAsString('GITHUB RESPONSE UPDATED', body, 'text/plain')
     }
     return message
 }
