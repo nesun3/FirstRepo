deb3aacac300732052a9f677c97c02dcc0ba36e7
nesun3 <surneogi14@gmail.com>
2023-09-28 18:43:57 +0530

    js updated

--- src/Test 01/src/main/resources/script/script1.js
+++ src/Test 01/src/main/resources/script/script1.js
@@ -21,6 +21,6 @@ function processData(message) {
     value = properties.get("oldProperty");
     message.setProperty("oldProperty", value + " modified");
     message.setProperty("newProperty", "newProperty");
-    console.log("Hello World! to JS");
+    console.log("uno dos tres");
     return message;
 }
